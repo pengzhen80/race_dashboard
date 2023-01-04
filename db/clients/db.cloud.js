c5e753a1c7a90fcec089b2c348a25c7e6195a88f
pengzhen <guopengzhen2015@icloud.com>
2023-01-04 15:26:04 +0800

    update

--- db/clients/db.cloud.js
+++ db/clients/db.cloud.js
@@ -1,12 +1,22 @@
 const { Client } = require('pg')
 
+// const pg_cloud_client = new Client(
+//     {
+//         database:"cloudrace",
+//         host: "skyracing.com.cn",
+//         port: "5432",
+//         user: "onlinemxcsk",
+//         password: "",
+//     }
+// );
+
 const pg_cloud_client = new Client(
     {
         database:"cloudrace",
-        host: "skyracing.com.cn",
+        host: "localhost",
         port: "5432",
-        user: "onlinemxcsk",
-        password: "",
+        user: "pengzhen",
+        password: "pengzhen",
     }
 );
 
