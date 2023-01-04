6f7d120eabe1caab7e4213e30faca9dce1945735
pengzhen <guopengzhen2015@icloud.com>
2023-01-04 15:31:42 +0800

    add db backup

--- db/clients/db.cloud.js
+++ db/clients/db.cloud.js
@@ -1,15 +1,6 @@
 const { Client } = require('pg')
 
-// const pg_cloud_client = new Client(
-//     {
-//         database:"cloudrace",
-//         host: "skyracing.com.cn",
-//         port: "5432",
-//         user: "onlinemxcsk",
-//         password: "",
-//     }
-// );
-
+//I hide cloud params for safe, use the local params
 const pg_cloud_client = new Client(
     {
         database:"cloudrace",
